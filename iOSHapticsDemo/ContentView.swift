691bf48f33f9b5c80f18287abe12ac15d09318f8
xthonik <64434276+xthonik@users.noreply.github.com>
2025-04-10 11:52:04 +0500

    Initial Commit

---
+++ iOSHapticsDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSHapticsDemo
+//
+//  Created by Darkhan Basshybayev on 10.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSHapticsDemo/iOSHapticsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSHapticsDemoApp.swift
+//  iOSHapticsDemo
+//
+//  Created by Darkhan Basshybayev on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct iOSHapticsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
